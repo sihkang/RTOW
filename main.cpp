0756ebed22ea096acc25ea232bcbcc3e98e3db15
Sihwan Kang <sihkang@c5r2s7.42seoul.kr>
2024-11-29 17:22:00 +0900

    2.3 : adding clog output

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ int main() {
     int image_height = 256;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++) {
-        for (int i = 0; i < image_width; i++) {
+    for (int j = 0; j < image_height; j++) 
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) 
+        {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.0;
@@ -24,4 +25,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rScanlines remaining: " << 0 << ' ' << std::flush;
+    std::clog << "\r\nDone:-)               \n\n";
 }
